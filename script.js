ecab13b332c81bc86468d470cc76375c9e22a713
kadijatsuleman <sandrasule479@gmail.com>
2026-02-12 15:39:45 +0100

    index.html, about.html, contact.html, script.js,
    style.css

---
+++ script.js
@@ -0,0 +1,10 @@
+// function showDelivered (event){
+//   event.preventdefault();
+// }
+// document.getElementById("contact-form").requestFullscreen();
+// document.getElementById("success-message").style.display ="block";
+// MessageChannel.scrollintroview({behavior:"smooth"})
+function  submitform(){}
+alert("message delivered")
+document.getElementById("contact-form"). reset();
+  document.getElementById("success-messages"). style.display="block"
